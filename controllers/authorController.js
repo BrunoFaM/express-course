666132258d056b4c685ba55968467f93d3cde009
Bruno M <brunomoron56@gmail.com>
2023-04-11 20:58:35 -0300

    inplemented create author

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,7 @@
 const Author = require("../models/author");
 const async = require("async");
-const Book = require('../models/book');
+const Book = require("../models/book");
+const { body, validationResult } = require("express-validator");
 
 // Display list of all Authors
 exports.author_list = (req, res, next) => {
@@ -22,40 +23,90 @@ exports.author_list = (req, res, next) => {
 exports.author_detail = (req, res, next) => {
   async.parallel(
     {
-      author(callback){
+      author(callback) {
         Author.findById(req.params.id).exec(callback);
       },
-      authors_books(callback){
-        Book.find({author: req.params.id}, 'title summary').exec(callback);
+      authors_books(callback) {
+        Book.find({ author: req.params.id }, "title summary").exec(callback);
       },
     },
-    (err, results) =>{
-      if(err){
+    (err, results) => {
+      if (err) {
         return next(err);
       }
-      if(results.author == null){
-        const err = new Error('Author not found');
+      if (results.author == null) {
+        const err = new Error("Author not found");
         err.status = 404;
         return next(err);
       }
-      res.render('author_detail', {
-        title: 'Author Detail',
+      res.render("author_detail", {
+        title: "Author Detail",
         author: results.author,
         author_books: results.authors_books,
-      })
+      });
     }
-  )
+  );
 };
 
 // Display Author create form on GET
-exports.author_create_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author create get");
+exports.author_create_get = (req, res, next) => {
+  res.render("author_form", { title: "Create Author" });
 };
 
 // Handle Author create on POST
-exports.author_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author create POST");
-};
+exports.author_create_post = [
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified."),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.render("author_form", {
+        title: "Create Author",
+        author: req.body,
+        errors: errors.array(),
+      });
+      return;
+    }
+    // Data from form is valid.
+
+    // Create an Author object with escaped and trimmed data.
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+    });
+    author.save((err) => {
+      if (err) {
+        return next(err);
+      }
+      // Successful - redirect to new author record.
+      res.redirect(author.url);
+    });
+  },
+];
 
 // Display Author delete form on GET
 exports.author_delete_get = (req, res) => {
